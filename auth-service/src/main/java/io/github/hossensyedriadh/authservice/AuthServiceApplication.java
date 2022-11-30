48acb6405686c4f0cf59acb8e612b0e35178ce46
hossensyedriadh <48594108+hossensyedriadh@users.noreply.github.com>
2022-12-01 02:58:55 +0600

    jwt public key fixed, eureka client configured

--- auth-service/src/main/java/io/github/hossensyedriadh/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/io/github/hossensyedriadh/authservice/AuthServiceApplication.java
@@ -2,10 +2,12 @@ package io.github.hossensyedriadh.authservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 import org.springframework.web.reactive.config.EnableWebFlux;
 
+@EnableEurekaClient
 @EnableR2dbcRepositories(basePackages = {"io.github.hossensyedriadh.authservice.repository.r2dbc"})
 @EnableReactiveMongoRepositories(basePackages = {"io.github.hossensyedriadh.authservice.repository.mongo"})
 @EnableWebFlux
