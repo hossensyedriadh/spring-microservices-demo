bcf85bd9faeffed2c600636f0b5da9072922784d
hossensyedriadh <48594108+hossensyedriadh@users.noreply.github.com>
2022-12-02 01:16:08 +0600

    eureka client configured

--- order-service/src/main/java/io/github/hossensyedriadh/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/io/github/hossensyedriadh/orderservice/OrderServiceApplication.java
@@ -2,10 +2,12 @@ package io.github.hossensyedriadh.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 import org.springframework.web.reactive.config.EnableWebFlux;
 
+@EnableEurekaClient
 @EnableFeignClients(basePackages = {"io.github.hossensyedriadh.orderservice.proxy"})
 @EnableWebFlux
 @EnableReactiveMongoRepositories(basePackages = {"io.github.hossensyedriadh.orderservice.repository"})
--- product-service/src/main/java/io/github/hossensyedriadh/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/io/github/hossensyedriadh/productservice/ProductServiceApplication.java
@@ -2,9 +2,11 @@ package io.github.hossensyedriadh.productservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 import org.springframework.web.reactive.config.EnableWebFlux;
 
+@EnableEurekaClient
 @EnableWebFlux
 @EnableReactiveMongoRepositories(basePackages = {"io.github.hossensyedriadh.productservice.repository"})
 @SpringBootApplication
