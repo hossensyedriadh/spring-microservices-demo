307c40624cd1895276df891d86f95ec56591ffcc
hossensyedriadh <48594108+hossensyedriadh@users.noreply.github.com>
2022-12-02 23:22:29 +0600

    switched to ReactiveFeign client

--- order-service/src/main/java/io/github/hossensyedriadh/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/io/github/hossensyedriadh/orderservice/OrderServiceApplication.java
@@ -3,12 +3,12 @@ package io.github.hossensyedriadh.orderservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 import org.springframework.web.reactive.config.EnableWebFlux;
+import reactivefeign.spring.config.EnableReactiveFeignClients;
 
 @EnableEurekaClient
-@EnableFeignClients(basePackages = {"io.github.hossensyedriadh.orderservice.proxy"})
+@EnableReactiveFeignClients(basePackages = {"io.github.hossensyedriadh.orderservice.proxy"})
 @EnableWebFlux
 @EnableReactiveMongoRepositories(basePackages = {"io.github.hossensyedriadh.orderservice.repository"})
 @SpringBootApplication
--- order-service/src/main/java/io/github/hossensyedriadh/orderservice/proxy/ProductServiceProxy.java
+++ order-service/src/main/java/io/github/hossensyedriadh/orderservice/proxy/ProductServiceProxy.java
@@ -1,13 +1,14 @@
 package io.github.hossensyedriadh.orderservice.proxy;
 
 import io.github.hossensyedriadh.orderservice.model.Product;
-import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import reactivefeign.spring.config.ReactiveFeignClient;
+import reactor.core.publisher.Mono;
 
-@FeignClient(name = "api-gateway-service")
+@ReactiveFeignClient(name = "edge-service")
 public interface ProductServiceProxy {
     @GetMapping(value = "/product-service/v1/products/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    Product retrieveProduct(@PathVariable("id") String id);
+    Mono<Product> retrieveProduct(@PathVariable("id") String id);
 }
