6d3335b4edb654be4c7fbaab54f37b37c334b407
Syed Riadh Hossen <48594108+hossensyedriadh@users.noreply.github.com>
2023-01-10 14:05:54 +0600

    webflux annotation removed

--- edge-service/src/main/java/io/github/hossensyedriadh/edgeservice/EdgeServiceApplication.java
+++ edge-service/src/main/java/io/github/hossensyedriadh/edgeservice/EdgeServiceApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.reactive.ReactiveUserDetailsServiceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.web.reactive.config.EnableWebFlux;
 
 @EnableEurekaClient
-@EnableWebFlux
 @SpringBootApplication(exclude = {ReactiveUserDetailsServiceAutoConfiguration.class})
 public class EdgeServiceApplication {
 
