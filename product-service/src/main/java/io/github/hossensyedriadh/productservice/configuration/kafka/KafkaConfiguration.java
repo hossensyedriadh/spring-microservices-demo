e497b4d7bd3a2bf5f1c25263d334851977cd0c87
Syed Riadh Hossen <48594108+hossensyedriadh@users.noreply.github.com>
2023-01-16 12:55:42 +0600

    kafka topic message processing updated

--- product-service/src/main/java/io/github/hossensyedriadh/productservice/configuration/kafka/KafkaConfiguration.java
+++ product-service/src/main/java/io/github/hossensyedriadh/productservice/configuration/kafka/KafkaConfiguration.java
@@ -1,6 +1,7 @@
 package io.github.hossensyedriadh.productservice.configuration.kafka;
 
 import io.github.hossensyedriadh.productservice.model.Order;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
@@ -9,6 +10,7 @@ import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.core.reactive.ReactiveKafkaConsumerTemplate;
 import reactor.kafka.receiver.ReceiverOptions;
 
+import java.util.Collections;
 import java.util.List;
 
 @EnableKafka
@@ -20,14 +22,27 @@ public class KafkaConfiguration {
     @Value("${kafka.consumer.topic.update-order}")
     private String updateOrderTopic;
 
-    @Bean
-    public ReceiverOptions<String, Order> receiverOptions(KafkaProperties kafkaProperties) {
+    @Bean("createOrderOptions")
+    public ReceiverOptions<String, Order> receiverOptionsCreateOrderTopic(KafkaProperties kafkaProperties) {
         ReceiverOptions<String, Order> receiverOptions = ReceiverOptions.create(kafkaProperties.buildConsumerProperties());
-        return receiverOptions.subscription(List.of(this.createOrderTopic, this.updateOrderTopic));
+        return receiverOptions.subscription(Collections.singleton(this.createOrderTopic));
     }
 
-    @Bean
-    public ReactiveKafkaConsumerTemplate<String, Order> reactiveKafkaConsumerTemplate(ReceiverOptions<String, Order> receiverOptions) {
+    @Bean("createOrderTemplate")
+    public ReactiveKafkaConsumerTemplate<String, Order> reactiveKafkaConsumerTemplateCreateOrderTopic(@Qualifier("createOrderOptions")
+                                                                                                          ReceiverOptions<String, Order> receiverOptions) {
+        return new ReactiveKafkaConsumerTemplate<>(receiverOptions);
+    }
+
+    @Bean("updateOrderOptions")
+    public ReceiverOptions<String, Order> receiverOptionsUpdateOrderTopic(KafkaProperties kafkaProperties) {
+        ReceiverOptions<String, Order> receiverOptions = ReceiverOptions.create(kafkaProperties.buildConsumerProperties());
+        return receiverOptions.subscription(Collections.singleton(this.updateOrderTopic));
+    }
+
+    @Bean("updateOrderTemplate")
+    public ReactiveKafkaConsumerTemplate<String, Order> reactiveKafkaConsumerTemplateUpdateOrderTopic(@Qualifier("updateOrderOptions")
+                                                                                                      ReceiverOptions<String, Order> receiverOptions) {
         return new ReactiveKafkaConsumerTemplate<>(receiverOptions);
     }
 }
--- product-service/src/main/java/io/github/hossensyedriadh/productservice/service/ProductOrderProcessingService.java
+++ product-service/src/main/java/io/github/hossensyedriadh/productservice/service/ProductOrderProcessingService.java
@@ -7,6 +7,7 @@ import io.github.hossensyedriadh.productservice.repository.ProductRepository;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.kafka.core.reactive.ReactiveKafkaConsumerTemplate;
 import org.springframework.stereotype.Service;
@@ -16,29 +17,47 @@ import reactor.core.publisher.Flux;
 public class ProductOrderProcessingService implements CommandLineRunner {
     private final Logger logger = Logger.getLogger(ProductOrderProcessingService.class);
 
-    private final ReactiveKafkaConsumerTemplate<String, Order> reactiveKafkaConsumerTemplate;
+    private final ReactiveKafkaConsumerTemplate<String, Order> createOrderKafkaTemplate;
+    private final ReactiveKafkaConsumerTemplate<String, Order> updateOrderKafkaTemplate;
     private final ProductRepository productRepository;
 
     @Autowired
-    public ProductOrderProcessingService(ReactiveKafkaConsumerTemplate<String, Order> reactiveKafkaConsumerTemplate,
+    public ProductOrderProcessingService(@Qualifier("createOrderTemplate") ReactiveKafkaConsumerTemplate<String, Order> createOrderKafkaTemplate,
+                                         @Qualifier("updateOrderTemplate") ReactiveKafkaConsumerTemplate<String, Order> updateOrderKafkaTemplate,
                                          ProductRepository productRepository) {
-        this.reactiveKafkaConsumerTemplate = reactiveKafkaConsumerTemplate;
+        this.createOrderKafkaTemplate = createOrderKafkaTemplate;
+        this.updateOrderKafkaTemplate = updateOrderKafkaTemplate;
         this.productRepository = productRepository;
     }
 
-    private Flux<Order> listen() {
-        return this.reactiveKafkaConsumerTemplate.receiveAutoAck().map(ConsumerRecord::value).doOnNext(order -> logger.info("New order in queue: " + order))
+    private Flux<Order> listenCreateOrderTopic() {
+        return this.createOrderKafkaTemplate.receiveAutoAck().map(ConsumerRecord::value).doOnNext(order -> logger.info("New order in queue: " + order))
                 .doOnError(throwable -> logger.error("Consumption error: " + throwable.getMessage()));
+    }
+
+    private Flux<Order> listenUpdateOrderTopic() {
+        return this.updateOrderKafkaTemplate.receiveAutoAck().map(ConsumerRecord::value).doOnNext(order -> logger.info("Order update in queue: " +order))
+                .doOnError(throwable -> logger.error("Consumption error: " +throwable.getMessage()));
     }
 
     @Override
     public void run(String... args) {
-        this.listen().flatMap(order -> {
+        this.listenCreateOrderTopic().flatMap(order -> {
             Flux<Product> productFlux = this.productRepository.findAllById(Flux.fromStream(order.getItems().stream().map(Item::getProductRef)));
             Flux<Item> itemFlux = Flux.fromIterable(order.getItems());
 
             return productFlux.flatMap(product -> itemFlux.flatMap(item -> {
                 product.setStock(product.getStock() - item.getQuantity());
+                return this.productRepository.save(product);
+            }));
+        }).subscribe();
+
+        this.listenUpdateOrderTopic().flatMap(order -> {
+            Flux<Product> productFlux = this.productRepository.findAllById(Flux.fromStream(order.getItems().stream().map(Item::getProductRef)));
+            Flux<Item> itemFlux = Flux.fromIterable(order.getItems());
+
+            return productFlux.flatMap(product -> itemFlux.flatMap(item -> {
+                product.setStock(product.getStock() + item.getQuantity());
                 return this.productRepository.save(product);
             }));
         }).subscribe();
